6de5a7fa520f897956aefc57d6be758ebc89c7c0
hk4649 <hiroto.kawai@i-Vinci.co.jp>
2022-01-27 20:25:46 +0900

    フォーマット実施

--- src/todo/GetList.java
+++ src/todo/GetList.java
@@ -12,17 +12,19 @@ import javax.servlet.http.HttpServletResponse;
 
 import todo.model.TodoModel;
 import todo.model.TodoObject;
+
 @WebServlet("/get_list")
 public class GetList extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		// 常にPOSTへ渡します。
 		doPost(request, response);
 	}
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		TodoModel model = new TodoModel();
 		List<TodoObject> list = model.getTodoList();
 		request.setAttribute("list", list);
-	  RequestDispatcher rd = request.getRequestDispatcher("/list.jsp");
-	  rd.forward(request, response);
+		RequestDispatcher rd = request.getRequestDispatcher("/list.jsp");
+		rd.forward(request, response);
 	}
 }
